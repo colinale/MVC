918031f740ab05a4db87a7ae74ee7c1475360032
colinale <colinale@buffalo.edu>
2022-08-24 20:55:33 -0400

    file

---
+++ Main/index.js
@@ -0,0 +1,9 @@
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
